2895ec973bf6fe3cbadd30b8fb9ff64fa76ec632
Felipe DR <felipe.corp7@gmail.com>
2025-06-08 15:06:21 -0300

    feat: register cookie and route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 
+import { usersRoute } from './routes'
+
 export const app = fastify()
+
+app.register(cookie)
+app.register(usersRoute, { prefix: 'users' })
